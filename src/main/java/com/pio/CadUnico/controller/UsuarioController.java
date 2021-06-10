37cc849f27929c96ffbee76c9d3c3e39a7e35a56
Rafael da Silva <rafael._silva@outlook.com.br>
2021-06-10 20:48:50 -0300

    ultimos ajustes

--- src/main/java/com/pio/CadUnico/controller/UsuarioController.java
+++ src/main/java/com/pio/CadUnico/controller/UsuarioController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.pio.CadUnico.model.Usuario;
 import com.pio.CadUnico.repository.UsuarioRepository;
@@ -33,7 +31,6 @@ public class UsuarioController {
 	@Autowired UsuarioService usuarioService;
 	@Autowired UsuarioRepository usuarioRepository;
 	
-	
 	@GetMapping("/listar")
 	public @ResponseBody List<Usuario> listar() {
 		return usuarioRepository.findAll();
@@ -42,11 +39,9 @@ public class UsuarioController {
 	@GetMapping("/{usuarioId}")
 	public ResponseEntity<Usuario> buscar(@PathVariable Long usuarioId) {
 		Optional<Usuario> usuario = usuarioRepository.findById(usuarioId);
-		
 		if(usuario.isPresent()) {
 			return ResponseEntity.ok(usuario.get());
 		}
-		
 		return ResponseEntity.notFound().build();
 	}
 	
@@ -58,13 +53,11 @@ public class UsuarioController {
 	
 	@PutMapping("/{usuarioId}")
 	public ResponseEntity<Usuario> atualizar(@Validated @RequestBody Usuario usuario, @PathVariable Long usuarioId){
-		
 		if(!usuarioRepository.existsById(usuarioId))
 			return ResponseEntity.notFound().build();
 		
 		usuario.setId(usuarioId);
 		usuario = usuarioService.salvar(usuario);
-		
 		return ResponseEntity.ok(usuario);
 	}
 
